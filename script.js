5333343142ec5e4a930c49d9d479e48cabc11e64
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-14 13:59:44 +0100

    snack 1 fatto

---
+++ script.js
@@ -0,0 +1,13 @@
+//Snack 1 inserisci 2 numeri e puoi compara con if
+let num1 = parseInt(prompt(`Inserisci numero 1: `));
+let num2 = parseInt(prompt(`Inserisci numero 2: `));
+if (num1 < num2) {
+    console.log(`Primo numero e piu grande: ${num2}`);
+}
+if (num2 < num1) {
+    console.log(`Secondo numero e piu grande: ${num1}`);
+}
+else {
+    if(num1==num2)
+    console.log(`Sonno iguali numeri: ${num1}=${num2}`);
+}
